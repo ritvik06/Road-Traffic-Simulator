4a10fa57ac2a085198b2cbf9d7a5636170db9318
ritvik06 <34479116+ritvik06@users.noreply.github.com>
2019-04-11 04:25:56 +0530

    Parser for rect.cpp added

--- Tester/rect.cpp
+++ Tester/rect.cpp
@@ -1,18 +1,21 @@
 #include <GL/glut.h>
-#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <string>
 
-float location1 = -10;
-float location2 = -10;
+using namespace std;
+float aspectRatio=10;
+int frame=0;
 
-void rectangle(int x,int y,float w,float h,char colour){
+void rectangle(float x,float y,float w,float h,char colour){
 	switch(colour){
-                case 'G': glColor3f(0.0,1.0,0.0);
-                break;
-                case 'R': glColor3f(1.0,0.0,0.0);
-                break;
-                case 'B': glColor3f(0.0,0.0,1.0);
-                break;
-                default: glColor3f(1.0,1.0,1.0);
+        case 'G': glColor3f(0.0,1.0,0.0);
+	      break;
+	      case 'R': glColor3f(1.0,0.0,0.0);
+	      break;
+	      case 'B': glColor3f(0.0,0.0,1.0);
+	      break;
+	      default: glColor3f(1.0,1.0,1.0);
 	}
 
           glBegin(GL_QUADS);
@@ -47,25 +50,79 @@ static void resize(int width, int height)
 
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity() ;
-}
+}  
 
 static void display(void)
 {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
-    road();
-    rectangle(location1,-2,2,1,'G');
-    location1 = location1+0.08;
-    rectangle(location2,2,2,1,'R');  
-    location2 = location2+0.05;
-//rectangle(-10,0,1,0.1,'W');
-//rectangle(-7,0,1,0.1,'W');
-//rectangle(-4,0,1,0.1,'W');
-//rectangle(-1,0,1,0.1,'W');
-//rectangle(2,0,1,0.1,'W');
-//rectangle(5,0,1,0.1,'W'); 
-//rectangle(8,0,1,0.1,'W');
-//rectangle(0,3,0.1,6,'W');
- 
+  fstream file;
+  string word;
+  float aspectRatio;
+  string Display1 = "DisplayInfo.txt";
+  file.open(Display1.c_str());
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
+
+  float Y_coordinate = 0, X_coordinate = 0, vehicle_length = 0,vehicle_width = 0;   
+  char colour = 'W'; 
+
+  file >> word;
+  file >> word;
+   file >> word;
+  // cout << word << endl;
+
+   aspectRatio = (stoi(word))/10;
+   // cout << aspectRatio << endl;
+  while(file >> word){
+      // cout << word << " " << endl;
+     if(word=="Time"){
+      file >> word;
+      // cout << word << endl;
+      // cout << word << endl;
+      if(stof(word)==frame){
+          file >> word;
+          if(word=="SIGNAL"){
+            file >> word;
+            cout << word << endl;
+            //add feature for red or green light;
+            while(word!="Time"){             
+              file >> word;
+              if(word=="Vehicle"){
+                file >> word;
+                file >> word;
+                file >> word;
+                colour = word.at(0);
+                cout << colour << endl;
+                file >> word; 
+                vehicle_length = stod(word);
+                file >> word;
+                vehicle_width = stod(word);
+                file >> word;
+                X_coordinate = stod(word);
+                file >> word; 
+                Y_coordinate = stod(word);
+                // cout << "Print Rectangle" << endl;
+                rectangle(X_coordinate ,Y_coordinate ,vehicle_width,vehicle_length,colour);
+              }
+            }            
+          }
+      
+      }
+     }
+
+  }
+
+  road();
+
+  file.close();
+  rectangle(-10,0,1,0.1,'W');
+  rectangle(-7,0,1,0.1,'W');
+  rectangle(-4,0,1,0.1,'W');
+  rectangle(-1,0,1,0.1,'W');
+  rectangle(2,0,1,0.1,'W');
+  rectangle(5,0,1,0.1,'W'); 
+  rectangle(8,0,1,0.1,'W');
+  rectangle(0,3,0.1,6,'W');
+  frame+=1;
+
    glutSwapBuffers();
 }
 
@@ -81,7 +138,7 @@ int main(int argc, char *argv[])
 {
     glutInit(&argc, argv);
     glutInitWindowSize(640,480);
-    glutInitWindowPosition(320,240);
+    glutInitWindowPosition(0,0);
 
     glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
 
@@ -89,11 +146,12 @@ int main(int argc, char *argv[])
 
     glutReshapeFunc(resize);
     glutDisplayFunc(display);
+    // cout << "aspectRatio- " << aspectRatio << endl; 
+
     glutIdleFunc(idle);
 
     glClearColor(0,0,0,0);
 
     glutMainLoop();
-
     return EXIT_SUCCESS;
 }
